51642a7c80686a295b1ff45f571206d62eb992ed
Yesitha dilmith <95668663+kgydilmith@users.noreply.github.com>
2024-09-26 19:19:00 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB =async() =>{
-    (await mongoose.connect('mongodb+srv://dilmithyesitha:12345678@cluster0.cw4fvfl.mongodb.net/food')).isObjectIdOrHexString(()=>console.log("DB connected"));
-    
-
-}
